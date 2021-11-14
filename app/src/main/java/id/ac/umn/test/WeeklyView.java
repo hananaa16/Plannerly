53ea9e6c97112fd3d385be21d163206ee7cbed64
Violyy <violy@gmail.com>
2021-11-14 15:26:43 +0700

    Initial Commit

---
+++ app/src/main/java/id/ac/umn/test/WeeklyView.java
@@ -0,0 +1,82 @@
+package id.ac.umn.test;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
+
+import id.ac.umn.test.databinding.ActivityWeeklyViewBinding;
+
+public class WeeklyView extends AppCompatActivity {
+
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityWeeklyViewBinding binding;
+    RecyclerView rvDaftarMusik;
+    WeeklyAdapter mAdapter;
+    LinkedList<SourcePlanner> daftarMusik= new LinkedList<>();
+    String title;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityWeeklyViewBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbar);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+//        isiDaftarMusik();
+
+        rvDaftarMusik = (RecyclerView) findViewById(R.id.rvDailyTask);
+        mAdapter = new WeeklyAdapter(this, daftarMusik);
+        rvDaftarMusik.setAdapter(mAdapter);
+        rvDaftarMusik.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+//    public void isiDaftarMusik(){
+//
+//        daftarMusik.add(new SourcePlanner("Rapat BPH Koor", "Rapat", "fire", "android.resource://" +getPackageName() + "/"+ R.raw.wowsfx));
+//        daftarMusik.add(new SourcePlanner("Tugas Akhir", "Tugas", "Makalah","android.resource://" +getPackageName() + "/"+ R.raw.kleeboom1));
+//
+//    }
+}
