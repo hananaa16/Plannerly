792bb26ba777819d751d304fee786357b32a5bf1
Violyy <violylislianty@gmail.com>
2021-11-28 14:09:08 +0700

    Initial Message

---
+++ app/src/main/java/id/ac/umn/gallerypicker/MainActivity.java
@@ -0,0 +1,123 @@
+package id.ac.umn.gallerypicker;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+
+public class MainActivity extends AppCompatActivity {
+public ImageView pick;
+public static final int CAMERA_REQUEST=100;
+public static final int STORAGE_REQUEST=101;
+String cameraPermission[];
+String storagePermission[];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    cameraPermission=new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    storagePermission=new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+    pick=(ImageView)findViewById(R.id.pickImage);
+    pick.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          int picd=0;
+          if(picd == 0){
+              if(!checkCameraPermission()){
+                  requestCameraPermission();
+              }else{
+                  pickFromGallery();
+              }
+          }else if(picd == 1){
+              if(!checkStoragePermission()){
+                  requestStoragePermission();
+              }else{
+                  pickFromGallery();
+              }
+          }
+        }
+    });
+
+    }
+
+    private void requestStoragePermission() {
+        requestPermissions(storagePermission,STORAGE_REQUEST);
+    }
+
+    private boolean checkStoragePermission() {
+        boolean result=ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE)==(PackageManager.PERMISSION_GRANTED);
+        return result;
+    }
+
+    private void pickFromGallery() {
+        CropImage.activity().start(this);
+    }
+
+    private void requestCameraPermission() {
+       requestPermissions(cameraPermission,CAMERA_REQUEST);
+    }
+
+    private boolean checkCameraPermission() {
+        boolean result = ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA)==(PackageManager.PERMISSION_GRANTED);
+        boolean result1 = ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE)==(PackageManager.PERMISSION_GRANTED);
+        return result && result1;
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+            CropImage.ActivityResult result=CropImage.getActivityResult(data);
+            if(resultCode==RESULT_OK){
+                Uri resultUri = result.getUri();
+                Picasso.with(this).load(resultUri).into(pick);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch(requestCode){
+            case CAMERA_REQUEST:{
+                if(grantResults.length>0){
+                    boolean camera_accepted=grantResults[0]==(PackageManager.PERMISSION_GRANTED);
+                    boolean storage_accepted=grantResults[1]==(PackageManager.PERMISSION_GRANTED);
+                    if(camera_accepted && storage_accepted){
+                        pickFromGallery();
+                    }else{
+                        Toast.makeText(this,"Camera & Storage Access Denied", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            break;
+            case STORAGE_REQUEST:{
+                if(grantResults.length>0){
+                    boolean storage_accepted = grantResults[0]==(PackageManager.PERMISSION_GRANTED);
+                    if(storage_accepted){
+                        pickFromGallery();
+                    }else{
+                        Toast.makeText(this,"Storage Access Denied", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
+    }
+}
